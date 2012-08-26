c4ef1362db3bd5af65341010ebe1c72c3a9fc0dc
Manuel Gomez <manuel.gomez@kodesoft.net>
2012-08-26 02:00:12 +0200

    Progressbar layout update issue solved with SwingWorkers and EventListeners. box and text colors fixed. cursor changes fixed. Pending to solve 1 problem: image ratio 4:3 / FF and the big monster: image loading speed.

--- src/descartesj/DescartesJ.java
+++ src/descartesj/DescartesJ.java
@@ -3,7 +3,7 @@
  * and open the template in the editor.
  */
 package descartesj;
-
+/*
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import javafx.application.Platform;
@@ -15,73 +15,77 @@ import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javax.swing.JApplet;
 import javax.swing.JFrame;
+*/
 import javax.swing.SwingUtilities;
+
 import javax.swing.UIManager;
 
 /**
  *
  * @author tupolev
  */
-public class DescartesJ extends JApplet {
-    
-    private static final int JFXPANEL_WIDTH_INT = 300;
-    private static final int JFXPANEL_HEIGHT_INT = 250;
-    private static JFXPanel fxContainer;
+//public class DescartesJ extends JApplet {
+  public class DescartesJ {  
+//    private static final int JFXPANEL_WIDTH_INT = 300;
+//    private static final int JFXPANEL_HEIGHT_INT = 250;
+//    private static JFXPanel fxContainer;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
         SwingUtilities.invokeLater(new Runnable() {
             @Override
             public void run() {
+                
                 try {
                     UIManager.setLookAndFeel("com.sun.java.swing.plaf.nimbus.NimbusLookAndFeel");
                 } catch (Exception e) {
                 }
-                
-                JFrame frame = new JFrame("JavaFX 2 in Swing");
-                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-                
-                JApplet applet = new DescartesJ();
-                applet.init();
-                
-                frame.setContentPane(applet.getContentPane());
-                
-                frame.pack();
-                frame.setLocationRelativeTo(null);
-                frame.setVisible(true);
-                
-                applet.start();
+                (new MainWindow()).show();
+//                JFrame frame = new JFrame("JavaFX 2 in Swing");
+//                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//                
+//                JApplet applet = new DescartesJ();
+//                applet.init();
+//                
+//                frame.setContentPane(applet.getContentPane());
+//                
+//                frame.pack();
+//                frame.setLocationRelativeTo(null);
+//                frame.setVisible(true);
+//                
+//                applet.start();
             }
         });
     }
     
-    @Override
-    public void init() {
-        fxContainer = new JFXPanel();
-        fxContainer.setPreferredSize(new Dimension(JFXPANEL_WIDTH_INT, JFXPANEL_HEIGHT_INT));
-        add(fxContainer, BorderLayout.CENTER);
-        // create JavaFX scene
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                createScene();
-            }
-        });
-    }
+//    @Override
+//    public void init() {
+//        fxContainer = new JFXPanel();
+//        fxContainer.setPreferredSize(new Dimension(JFXPANEL_WIDTH_INT, JFXPANEL_HEIGHT_INT));
+//        add(fxContainer, BorderLayout.CENTER);
+//        // create JavaFX scene
+//        Platform.runLater(new Runnable() {
+//            @Override
+//            public void run() {
+//                createScene();
+//            }
+//        });
+//    }
     
-    private void createScene() {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        fxContainer.setScene(new Scene(root));
-    }
+//    private void createScene() {
+//        Button btn = new Button();
+//        btn.setText("Say 'Hello World'");
+//        btn.setOnAction(new EventHandler<ActionEvent>() {
+//            @Override
+//            public void handle(ActionEvent event) {
+//                System.out.println("Hello World!");
+//            }
+//        });
+//        StackPane root = new StackPane();
+//        root.getChildren().add(btn);
+//        fxContainer.setScene(new Scene(root));
+//    }
 }
